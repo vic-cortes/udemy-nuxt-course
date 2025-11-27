9e15aaa02f82fc6152ca8d6a5c8fffe1f6720561
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-11-27 00:10:27 -0600

    14. Route Params & Links in Nuxt
    - https://www.udemy.com/course/master-nuxt-full-stack-complete-guide/learn/lecture/39124110#overview

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Blog Post {{ route.params.id }}</h1>
+    <p>This is the blog post content.</p>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
---
+++ app/pages/blog/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the Nuxt.js Course!</h1>
+    <p>These are all the blogs.</p>
+  </div>
+</template>
