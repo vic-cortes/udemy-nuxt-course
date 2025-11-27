60a777e411c7dac7469625fd91e9dca9ade7a39e
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-11-27 00:01:11 -0600

    13. Pages & Routing in Nuxt
    - https://www.udemy.com/course/master-nuxt-full-stack-complete-guide/learn/lecture/39124106#overview

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
 <template>
+  <nav>
+    <ul>
+      <li><NuxtLink to="/">Home</NuxtLink></li>
+      <li><NuxtLink to="/about">About</NuxtLink></li>
+    </ul>
+  </nav>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the Nuxt.js Course!</h1>
+    <p>This is the about page.</p>
+  </div>
+</template>
