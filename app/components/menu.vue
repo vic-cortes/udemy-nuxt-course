408990579fd56cfb558fd37fb879a30109791b98
Victor M. Cortes Figueroa <vmcortesf@gmail.com>
2025-11-30 09:45:48 -0600

    21. Data Fetching
    - https://www.udemy.com/course/master-nuxt-full-stack-complete-guide/learn/lecture/39222716#overview

--- app/components/menu.vue
+++ app/components/menu.vue
@@ -3,6 +3,7 @@
     <ul class="flex space-x-4">
       <li><NuxtLink to="/" class="link">Home</NuxtLink></li>
       <li><NuxtLink to="/about" class="link">About</NuxtLink></li>
+      <li><NuxtLink to="/projects" class="link">Projects</NuxtLink></li>
     </ul>
   </nav>
 </template>
---
+++ app/components/project-list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Projects</div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Error: {{ error.message }}</section>
+  <section v-else>display all data</section>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch(
+  'https://api.github.com/users/vic-cortes/repos'
+);
+</script>
