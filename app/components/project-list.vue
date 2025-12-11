a48f8c872e53ea34d463d232f7d0ef3cd4020ef5
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-12-11 12:14:13 -0500

    22. Data Fetching: Project List - Listing Your GitHub
    - https://www.udemy.com/course/master-nuxt-full-stack-complete-guide/learn/lecture/39222718#overview

--- app/components/project-list.vue
+++ app/components/project-list.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>Projects</div>
+  <div class="mb-10">Projects</div>
 
   <section v-if="pending">Loading...</section>
   <section v-else-if="error">Error: {{ error.message }}</section>
-  <section v-else>display all data</section>
+  <section v-else>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repository in repos"
+        :key="repository.id"
+        class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+      >
+        <a :href="repository.html_url" target="_blank">
+          <div class="flex items-center justify-between">
+            <div class="font-semibold">
+              {{ repository.name }}
+            </div>
+            <div>{{ repository.stargazers_count }} ⭐</div>
+          </div>
+          <p class="text-sm">{{ repository.description }}</p>
+        </a>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
 const { error, pending, data } = await useFetch(
   'https://api.github.com/users/vic-cortes/repos'
+);
+
+const repos = computed(() =>
+  data.value
+    .filter((repo) => repo.description)
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
 );
 </script>
